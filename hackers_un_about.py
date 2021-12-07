3ae8f37f41af76a889775bc2b8e8f20fcc50fc04
Hackers United <hackers1united@gmail.com>
2021-12-07 04:17:33 +0100

    Add files via upload

---
+++ hackers_un_about.py
@@ -0,0 +1,178 @@
+import time as t
+import sys
+import os
+import webbrowser
+t.sleep(3)
+os.system("clear")
+
+print("""
+
++-+-+-+-+-+-+-+-+-+-+-+-+-+
+|H|A|C|K|E|R|S|U|N|I|T|E|D
++-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+""" )
+
+print("""
+Tool name: Hackers United Biography
+Version: 1.0
+Creator: Teniola Ayodele
+Team: Hackers United
+Github: https://github.com/Hackers-Unit
+WhatsApp: +2348164064452
+""")
+
+print("""
+
+1. What is Hackers United?
+2. Who is the CEO of Hackers United?
+3. When was Hackers United Founded?
+4. How to Join Hackers United.
+5. Is Hackers United free?
+6. Contact Hackers United
+""")
+
+print("Select an Option")
+t = input("~#: ")
+os.system("clear")
+if t == "1":
+    print("""
+
++-+-+-+-+-+-+-+-+-+-+-+-+-+
+|H|A|C|K|E|R|S|U|N|I|T|E|D
++-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+""" )
+
+    
+    print("""
+Hackers United is a Group,
+Cyber Team that fight against corruption, scam stuff, theft.
+""")
+elif t == "2":
+    print("""
+
++-+-+-+-+-+-+-+-+-+-+-+-+-+
+|H|A|C|K|E|R|S|U|N|I|T|E|D
++-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+""" )
+
+    
+    print("""
+The CEO of Hackers United is:
+       Mayherms Wurld
+""")
+elif t == "3":
+    print("""
+
++-+-+-+-+-+-+-+-+-+-+-+-+-+
+|H|A|C|K|E|R|S|U|N|I|T|E|D
++-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+""" )
+
+    
+    print("""
+Hackers United was Created on the:
+2nd of November 2020. By:
+        Mayherms Wurld
+""")
+elif t == "4":
+    print("""
+
++-+-+-+-+-+-+-+-+-+-+-+-+-+
+|H|A|C|K|E|R|S|U|N|I|T|E|D
++-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+""" )
+
+    
+    print("""
+To Join Hackers United, you need to follow these steps below:
+1. Visit our Website at https://hackersunited.ga.
+2. Sign Up and fill in all the necessary Fields.
+3. After Signing Up Fill in all your Social Media links On Your Profile.
+
+Within 24 hours, you will be added to our Social Media Platforms
+Note: Facebook Social Media link must be Inputed on your Profile
+""")
+elif t == "5":
+    print("""
+
++-+-+-+-+-+-+-+-+-+-+-+-+-+
+|H|A|C|K|E|R|S|U|N|I|T|E|D
++-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+""" )
+
+    
+    print("""
+Yes!!!
+Hackers United is free, safe amd trusted.
+
+The only Purchasable items in Hackers United are:
+1. Custom Made Python Script
+2. Professional Phishing Pages
+3. Andriod Devices Tools like Cardro Pro and Activation Code.
+4. Private Teacher
+
+All items Listed above are sold on Hackers United Website at:
+https://hackersunited.ga/shop
+""")
+elif t == "6":
+    print("""
+
++-+-+-+-+-+-+-+-+-+-+-+-+-+
+|H|A|C|K|E|R|S|U|N|I|T|E|D
++-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+""" )
+
+    
+    print("""
+For Complaint or Enquires you can contact us at:
+
+Github: https://github.com/Hackers-Unit
+Website: https://hackersunited.ga/contact
+Email: hackers1united@gmail.com
+Whatsapp: +2348164064452
+
+""")
+t2 = input("Go Back to Main Menu? Y/N or Enter G to Visit Hackers United Website: ")
+if t2 == "Y":
+  os.system("python3 hackers_un_about.py")
+elif t2 == "y":
+  os.system("python3 hackers_un_about.py")
+if t2 == "N":
+  exit()
+elif t2 == "n":
+  exit()
+if t == "G":
+    print("""
+
++-+-+-+-+-+-+-+-+-+-+-+-+-+
+|H|A|C|K|E|R|S|U|N|I|T|E|D
++-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+""" )
+
+    
+    print("""
+Loading!!!!
+""")
+webbrowser.open("https://hackersunited.ga")
+if t == "g":
+    print("""
+
++-+-+-+-+-+-+-+-+-+-+-+-+-+
+|H|A|C|K|E|R|S|U|N|I|T|E|D
++-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+""" )
+
+    
+    print("""
+Loading!!!!
+""")
+webbrowser.open("https://hackersunited.ga")
